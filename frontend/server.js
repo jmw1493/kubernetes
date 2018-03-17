3894f841cebb76469ef77384aa00c1a056b5d36c
sarahheacock <seheacock@bellsouth.net>
2018-03-16 20:43:07 -0400

    merged

--- frontend/server.js
+++ frontend/server.js
@@ -4,13 +4,13 @@ const path = require('path');
 const request = require('request');
 
 app.get('/', (req, res, next) => {
-  // res.sendFile(path.resolve(__dirname, './index.html'))
+  res.sendFile(path.resolve(__dirname, './index.html'))
   // console.log("hello");
-  request.get('http://my-service:3000 ', (err, http, body) => {
-    console.log(err, http, body);
-    res.send(body);
-    // res.sendFile(path.resolve(__dirname, './index.html'))
-  })
+  // request.get('http://my-service:3000 ', (err, http, body) => {
+  //   console.log(err, http, body);
+  //   res.send(body);
+  //   // res.sendFile(path.resolve(__dirname, './index.html'))
+  // })
 })
 
 const PORT = process.env.PORT || 8080;
